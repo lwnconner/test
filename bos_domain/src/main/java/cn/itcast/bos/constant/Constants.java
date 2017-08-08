52d09b364d725039df1a707d909455d2cfb995d8
conner <bos@conner.cn>
2017-08-08 11:33:24 +0800

    第一次提交
    Signed-off-by: conner <bos@conner.cn>

---
+++ bos_domain/src/main/java/cn/itcast/bos/constant/Constants.java
@@ -0,0 +1,19 @@
+package cn.itcast.bos.constant;
+
+/**
+ * 常量类,用来定义http路径
+ * 
+ * @author 李吴楠 18717344828
+ * @version 1.0' 2017-7-16 13:39:03
+ */
+public class Constants {
+
+	/** 后台常量url */
+	public static final String BOS_MANAGEMENT_URL = "http://localhost:9001";
+	
+	/** 客户信息系统url */
+	public static final String CRM_MANAGEMENT_URL = "http://localhost:9002/crm_management";
+	
+	public static final String BOS_FORE_URL = "http://localhost:9003/bos_fore";
+	
+}
